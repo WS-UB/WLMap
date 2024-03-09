d3e2bdecca94904a122330282c69e04f867655cb
datischris <christopher.varghese@hotmail.com>
2024-03-08 19:30:34 -0500

    Allows basic user interaction to zoom rotate and pan

--- app/src/main/java/com/example/wlmap/MainActivity.kt
+++ app/src/main/java/com/example/wlmap/MainActivity.kt
@@ -1,52 +1,73 @@
 package com.example.wlmap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.res.Resources
 import android.os.Bundle
-import com.mapbox.bindgen.Value
-import com.mapbox.common.MapboxOptions
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
-import com.mapbox.maps.extension.style.expressions.dsl.generated.rgb
-import com.mapbox.maps.extension.style.layers.addLayer
-import com.mapbox.maps.extension.style.layers.generated.lineLayer
-import com.mapbox.maps.extension.style.sources.addSource
-import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
-import com.mapbox.maps.extension.style.sources.generated.vectorSource
-import com.mapbox.maps.extension.style.style
+import com.mapbox.maps.extension.style.layers.generated.FillLayer
+import com.mapbox.maps.extension.style.layers.generated.LineLayer
+import com.mapbox.maps.extension.style.layers.getLayerAs
 import com.mapbox.maps.plugin.gestures.gestures
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MapboxOptions.accessToken = "pk.eyJ1IjoiY2d2YXJnaGUiLCJhIjoiY2x0ZGNwMDEzMDVhMzJrc2hpeW42NG9kbyJ9.iLZBux7plgn3t4yA2g5YOQ"
-        setContentView(R.layout.activity_main)
 
-        // Create a map programmatically and set the initial camera
+        //setContentView(R.layout.activity_main)
+
+        // Create a vertical LinearLayout to hold the MapView and buttons
+        val container = RelativeLayout(this)
+
         val mapView = MapView(this)
         mapView.mapboxMap.setCamera(
             CameraOptions.Builder()
-                .center(Point.fromLngLat(-78.7873, 43.0028))
+                .center(Point.fromLngLat(LONGITUDE, LATITUDE))
                 .pitch(0.0)
-                .zoom(17.5)
+                .zoom(ZOOM)
                 .bearing(0.0)
                 .build()
         )
         // Add the map view to the activity (you can also add it to other views as a child)
-        setContentView(mapView)
+        //setContentView(mapView)
 
+        container.addView(mapView)
+
+        setContentView(container)
+
+
+        // Set the content view to the container
+        setContentView(container)
+
+        mapView.gestures.doubleTapToZoomInEnabled = true
         mapView.gestures.rotateEnabled = true
         mapView.gestures.pinchToZoomEnabled = true
 
-        mapView.mapboxMap.loadStyle(
-            style(style = Style.STANDARD) {
-                +geoJsonSource(id = "floor-1") {
-                    data("asset://floor1-davis.geojson")
-                }
-            }
-        )
+        mapView.mapboxMap.loadStyle(style = STYLE_CUSTOM)
+
     }
 
+    companion object {
+        private const val STYLE_CUSTOM = "asset://style.json"
+        private const val FLOOR1_LAYOUT = "davis01-poly-46dzby"
+        private const val FLOOR3_LAYOUT = "davis03-aef7zh"
+        private const val LATITUDE = 43.0028
+        private const val LONGITUDE = -78.7873
+        private const val ZOOM = 17.5
+    }
+
+    private fun Int.dpToPx(): Int {
+        val density = Resources.getSystem().displayMetrics.density
+        return (this * density).toInt()
+    }
 }
