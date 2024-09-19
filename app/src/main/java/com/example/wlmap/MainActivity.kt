15ec5cd0dba161302b01e5eca078b8ab59be35c8
HarryM3103 <hemoore3@buffalo.edu>
2024-09-19 19:02:09 -0400

    WLMap is displayed in the appbar

--- app/src/main/java/com/example/wlmap/MainActivity.kt
+++ app/src/main/java/com/example/wlmap/MainActivity.kt
@@ -1,30 +1,9 @@
 package com.example.wlmap
 
 import LocationPermissionHelper
-import android.R
-import android.content.ContentValues
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
 import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.PopupWindow
-import android.widget.RelativeLayout
-import android.widget.SearchView
-import android.widget.Spinner
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -32,88 +11,14 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
-import com.mapbox.common.location.AccuracyLevel
-import com.mapbox.common.location.DeviceLocationProvider
-import com.mapbox.common.location.IntervalSettings
-import com.mapbox.common.location.Location
-import com.mapbox.common.location.LocationObserver
-import com.mapbox.common.location.LocationProviderRequest
-import com.mapbox.common.location.LocationService
-import com.mapbox.common.location.LocationServiceFactory
-import com.mapbox.geojson.Point
-import com.mapbox.geojson.Polygon
-import com.mapbox.maps.CameraOptions
-import com.mapbox.maps.MapView
-import com.mapbox.maps.RenderedQueryGeometry
-import com.mapbox.maps.RenderedQueryOptions
-import com.mapbox.maps.ScreenBox
-import com.mapbox.maps.extension.style.expressions.generated.Expression
-import com.mapbox.maps.extension.style.layers.generated.SymbolLayer
-import com.mapbox.maps.extension.style.layers.generated.FillLayer
-import com.mapbox.maps.extension.style.layers.getLayerAs
-import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
-import com.mapbox.maps.plugin.animation.flyTo
-import com.mapbox.maps.plugin.annotation.AnnotationPlugin
-import com.mapbox.maps.plugin.annotation.annotations
-import com.mapbox.maps.plugin.annotation.generated.CircleAnnotation
-import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationManager
-import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
-import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotation
-import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationManager
-import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
-import com.mapbox.maps.plugin.annotation.generated.createCircleAnnotationManager
-import com.mapbox.maps.plugin.annotation.generated.createPolylineAnnotationManager
-import com.mapbox.maps.plugin.gestures.addOnMapClickListener
-import com.mapbox.maps.plugin.gestures.gestures
-import com.mapbox.maps.toCameraOptions
-import org.eclipse.paho.client.mqttv3.MqttException
 import java.lang.ref.WeakReference
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.pow
-import kotlin.math.sin
-import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity()
 ,NavigationView.OnNavigationItemSelectedListener
 {
-    private val serverUri = "tcp://128.205.218.189:1883"
-    private val clientId = "Client ID"
-    private val serverTopic = "receive-wl-map"
-    private val STYLE_CUSTOM = "asset://style.json"
-    private val FLOOR1_LAYOUT = "davis01"
-    private val FLOOR1_LABELS = "davis01labels"
-    private val FLOOR1_DOORS = "davis01doors"
-    private val FLOOR3_LABELS = "davis03labels"
-    private val FLOOR3_LAYOUT = "davis03"
-    private val FLOOR3_DOORS = "davis03doors"
-    private val spinnerOptions = listOf("Select", "All", "Room", "Bathroom", "Staircase", "Elevator")
-    private val LATITUDE = 43.0028
-    private val LONGITUDE = -78.7873
-    private val ZOOM = 17.9
-    private val testUserLocation = Point.fromLngLat(-78.78755328875651, 43.002534795993796)
-
-    private lateinit var mqttHandler: MqttHandler
     private lateinit var locationPermissionHelper: LocationPermissionHelper
-    private lateinit var annotationAPI: AnnotationPlugin
-    private lateinit var userAnnotationManager: CircleAnnotationManager
-    private lateinit var doorAnnotationManager: CircleAnnotationManager
-    private lateinit var polylineAnnotationManager: PolylineAnnotationManager
-    private lateinit var mapView: MapView
-    private lateinit var buttonF1: Button
-    private lateinit var buttonF3: Button
-    private lateinit var popupWindow: PopupWindow
-    private lateinit var userLastLocation: Point
     private lateinit var drawerLayout: DrawerLayout
 
-    //private var curRoute: List<Point> = null
-    private var doorSelected: Point? = null
-    private var circleAnnotationId: CircleAnnotation? = null
-    private var prevRoute: PolylineAnnotation? = null
-    private var routeDisplayed: Boolean = false
-    private var prevDoor: Boolean = false
-    private var lastLocation: Pair<Double, Double>? = null
-    private var floorSelected: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
