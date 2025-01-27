0411e8b8fd2372b7a18968b518c78499c7c6add6
yhahhh <huyang616982084@gmail.com>
2025-01-27 17:59:04 -0500

    Debugging import errors

--- app/src/main/java/com/example/wlmap/MapFragment.kt
+++ app/src/main/java/com/example/wlmap/MapFragment.kt
@@ -14,6 +14,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.LocationManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Looper
@@ -41,6 +42,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import android.location.LocationListener
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
