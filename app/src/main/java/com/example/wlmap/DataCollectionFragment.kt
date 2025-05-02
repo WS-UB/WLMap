1ed43947c483b231d5c1d82cb1b75cca3b7c2656
Harrison Moore <hemoore3@buffalo.edu>
2025-05-02 15:46:02 -0400

    Raw GPS Data is sent to MQTT

--- app/src/main/java/com/example/wlmap/DataCollectionFragment.kt
+++ app/src/main/java/com/example/wlmap/DataCollectionFragment.kt
@@ -1913,7 +1913,7 @@ class DataCollectionFragment : Fragment(),NavigationView.OnNavigationItemSelecte
                         val timeStamp = Timestamp(currentTimeMillis).toString()
                         val latitude_GPS = result?.latitude
                         val longitude_GPS = result?.longitude
-                        //mqttHandler.publish("/gps", "GPS_RAW,$randomDeviceID,$timeStamp, $latitude_GPS, $longitude_GPS")
+                        mqttHandler.publish("/gps", "GPS_RAW,$randomDeviceID,$timeStamp, $latitude_GPS, $longitude_GPS")
                     }
                     lastUpdate = actualTime
                 } //The way the readings are set up to be published is just a test
